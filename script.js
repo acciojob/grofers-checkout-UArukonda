591446920365dd2c3fc25895f82ae272e88c0cec
Upender Arukonda <109522168+UArukonda@users.noreply.github.com>
2026-06-17 00:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+  let sum = 0;
+  prices.forEach((node) => {
+    sum += Number(node.innerText);
+  });
+
+  const totalRow = document.createElement("tr");
+
+  const totalTh = document.createElement("td");
+  const total = document.createElement("td");
+  totalTh.innerText = "Total";
+  total.innerText = sum;
+
+  totalRow.appendChild(totalTh);
+  totalRow.appendChild(total);
+
+  const table = document.querySelector("table");
+
+  totalTh.classList.add = "item";
+  table.appendChild(totalRow);
   
 };
 
